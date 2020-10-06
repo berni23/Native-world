a77639023eab71f81d225d80fcb9cfddc98d9403
Berni <bernatferrerm@gmail.com>
2020-10-07 01:22:44 +0200

    bug revive user solved

--- user.js
+++ user.js
@@ -1,42 +1,26 @@
 function getUsers() {
-
     var localUsers = JSON.parse(localStorage.getItem('users'));
-
-    console.log(localUsers);
-
     if (localUsers) return reviveUsersObject(localUsers);
-
     return new users();
-
-
 }
 
 function reviveUsersObject(oldUsers) {
-
     var newUsers = new users();
-
     oldUsers.getUser = newUsers.getUser;
     oldUsers.setUser = newUsers.setUser;
     oldUsers.userExist = newUsers.userExist;
     oldUsers.save = newUsers.save;
-
     Object.keys(oldUsers.userList).forEach(function (user) {
-        user = reviveUser(user);
-
+        reviveUser(oldUsers.userList[user]);
     });
-
     return oldUsers;
-
 
 }
 
-
 function reviveUser(deadUser) {
     var newUser = new user('', '');
-    deadUser.addLanguage = newUser.addLanguage;
+    deadUser.addLanguage = newUser.addLanguage
     return deadUser;
-
-
 }
 class users {
     constructor() {
@@ -47,22 +31,14 @@ class users {
         this.setUser = function (userName, password) {
             if (this.userExist(userName)) return false; // user exists
             this.userList[userName] = new user(userName, password);
-
-
-            console.log('setUser', this);
         };
         this.userExist = function (userName) {
             return this.userList[userName] == true;
         };
         this.save = function () {
-
-            console.log('saveObject', this);
             localStorage.setItem("users", JSON.stringify(this));
-            console.log(JSON.parse(localStorage.getItem("users")));
         };
     }
-
-
 }
 
 class user {
@@ -74,10 +50,7 @@ class user {
         this.addLanguage = function (language) {
             this.languages[language] = {
                 example1: {}
-
-
             };
-
         }
     }
 }
