03c260bf86953b736fc54bf2c7a190ee660112d3
Berni <bernatferrerm@gmail.com>
2020-10-07 13:47:34 +0200

    code  cleaned and optimized

--- user.js
+++ user.js
@@ -65,12 +65,3 @@ class user {
 
 
 }
-
-
-
-
-/*var encryptedAES = CryptoJS.AES.encrypt("Message", "My Secret Passphrase");
-var decryptedBytes = CryptoJS.AES.decrypt(encryptedAES, "My Secret Passphrase");
-var plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
-
-*/
